125fcc1660694a467401c1e10ffc2f785b86164a
DDQ <dudeqing@live.com>
2015-11-06 16:24:02 +0800

    excel

---
+++ subprojects/poi/src/main/java/du/core/excle/ReadExcel.java
@@ -0,0 +1,72 @@
+package du.core.excle;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.math.BigDecimal;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.junit.Test;
+
+public class ReadExcel {
+
+	/**
+	 * 好像XSSFWorkbook取消了
+	 * 
+	 * 要变成多线程 读多数据
+	 * @throws IOException
+	 */
+	@Test
+	public void read_97_2003() throws IOException {
+		
+		FileInputStream fileInputStream = new FileInputStream("C:\\Users\\Administrator\\Desktop\\7月三国杀发码.xls");
+		
+		HSSFWorkbook wookbook = new HSSFWorkbook(fileInputStream); 
+		
+		try{
+			HSSFSheet sheet = wookbook.getSheetAt(0);
+			
+			// 行列都是从0开始
+			int rows = sheet.getPhysicalNumberOfRows(); // 获取到Excel文件中的所有行数
+			
+			System.out.println(rows);
+			int cells = 3;
+			
+			for(int i = 0; i < rows; ++i){
+				
+				HSSFRow row = sheet.getRow(i);
+				
+				if(row == null) continue;
+				
+				for(int j = 0; j < cells; ++j){
+					
+					HSSFCell cell = row.getCell(j);
+					
+					switch (cell.getCellType()) {
+					
+						case Cell.CELL_TYPE_STRING:
+							
+							System.out.println(cell.getStringCellValue());
+							break;
+							
+						case Cell.CELL_TYPE_NUMERIC:
+						case Cell.CELL_TYPE_FORMULA:
+							
+							System.out.println(new BigDecimal(cell.getNumericCellValue()).toPlainString());
+							break;
+	
+						default:
+							break;
+					}
+				}
+			}
+		}catch(Exception e){
+			throw e;
+		}finally {
+			wookbook.close();
+		}
+	}
+}
